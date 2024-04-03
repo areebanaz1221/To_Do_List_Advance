46d32da57c9bb4e22bdaa334a354cd7cd48fec97
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-03 20:21:26 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todoList = [];
+let conditions = true;
+
+console.log(chalk.magenta.italic("\n \t Wellcome to Code With Areeba - Todo-List Application\n"));
+
+while(conditions){
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.green ("Enter your New Task :")
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task added in Todo-List successfully`);
+
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more task ?",
+            default: "False"
+        }
+    ]);
+    conditions = addMoreTask.addmore;
+
+}
+console.log("Your updated Todo-List", todoList);
